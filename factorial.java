d9c7674fdaa897174fa57e96da22856fbd787c97
Purnima Panda <purnimapanda08112003@gmail.com>
2025-05-16 16:14:52 +0530

    Delete factorial.java

--- factorial.java
+++
@@ -1,9 +0,0 @@
-public class factorial {
-    public static void main(String[] args) {
-        int n=8, result=1;
-        for(int i=1;i<=n;i++){
-             result=result*i;
-        }
-        System.out.println(result);
-    }
-}
